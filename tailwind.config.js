32432fda340640c136009846c71b8ab2a470c908
Blair Habicht <blairh@jellycode.com>
2022-12-29 20:30:12 -0600

    CSS cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,8 @@ module.exports = {
   './snippets/**/*.liquid',
   './templates/**/*.{json,liquid}',
  ],
-	theme: {
-		fontFamily: {
-      'sans': ['ui-sans-serif', 'system-ui', ...],
-      'serif': ['ui-serif', 'Georgia', ...],
-      'mono': ['ui-monospace', 'SFMono-Regular', ...],
-      'display': ['Oswald', ...],
-      'body': ['"Open Sans"', ...],
-    }
+ theme: {
+  fontFamily: {},
   container: {
    padding: {
     DEFAULT: '1rem',
