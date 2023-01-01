b2f8e267369a54a035252264ebdcb67d60246765
Blair Habicht <blairh@jellycode.com>
2023-01-01 14:48:01 -0600

    Style updates, switching to vite

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,24 @@
+import babel from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
 import css from 'rollup-plugin-import-css';
 
 export default {
- input: 'src/compare-slider.js',
+ input: 'src/main.js',
  output: {
-  file: 'assets/compare-slider.js',
+  file: 'assets/main.js',
   format: 'iife',
+  sourcemap: true,
  },
- plugins: [nodeResolve(), css()],
+ plugins: [
+  nodeResolve(),
+  css(),
+  replace({
+   'process.env.NODE_ENV': JSON.stringify('development'),
+  }),
+  babel({
+   presets: ['@babel/preset-react'],
+  }),
+ ],
 };
