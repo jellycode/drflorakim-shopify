9159f976acd75a5a7516d4a88ac2b17431572676
Blair Habicht <blairh@jellycode.com>
2023-01-19 12:00:03 -0500

    Service page and list update

--- src/components/ServicesList.vue
+++ src/components/ServicesList.vue
@@ -23,7 +23,7 @@ const {
 const services = computed(() => {
  if (data.value) {
   const servicesjson = JSON.parse(data.value);
-  console.log(servicesjson);
+
   return servicesjson.data;
  }
 });
@@ -41,14 +41,23 @@ const serviceContent = () => {
  return subCategories.value[currentSubTab.value]?.content;
 };
 
-const serviceImage = () => {
- if (!subCategories.value[currentSubTab.value]?.image) {
+const getImage = (img) => {
+ if (!img) {
+  return props.background;
+ }
+ let image = 'https://admin.drflora.kim/images/' + img;
+
+ return image;
+};
+
+const serviceImage = (index) => {
+ if (!services.value[index]?.image) {
   return props.background;
  }
  let image =
   'https://admin.drflora.kim/images/' +
   subCategories.value[currentSubTab.value].image;
- console.log(image);
+
  return image;
 };
 
@@ -60,3 +69,31 @@ onMounted(() => {
  execute();
 });
 </script>
+
+<template>
+ <div
+  v-if="isFinished"
+  class="container m-auto border-0 lazy-bg"
+  v-for="(service, index) in services"
+  :key="service.slug"
+ >
+  <h3 class="mb-10">{{ service.name }}</h3>
+  <div class="grid grid-cols-1 md:grid-cols-2 gap-10 mb-20">
+   <div
+    v-for="(subservice, subindex) in service.services"
+    :key="subservice.name"
+    data-aos="fade-up"
+    data-aos-delay="50"
+   >
+    <div class="h-72 w-full mb-6">
+     <img
+      class="object-cover w-full h-full"
+      :src="getImage(subservice.image)"
+     />
+    </div>
+    <h4 class="mb-2">{{ subservice.name }}</h4>
+    <div v-html="subservice.content"></div>
+   </div>
+  </div>
+ </div>
+</template>
